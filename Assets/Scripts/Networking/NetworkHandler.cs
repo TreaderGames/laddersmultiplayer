2720c560326881f3a6f75677f41685aa40e35da0
Aziz_L <azizlokhandwala12@gmail.com>
2023-06-03 17:26:37 +0530

    Add adding start game

--- Assets/Scripts/Networking/NetworkHandler.cs
+++ Assets/Scripts/Networking/NetworkHandler.cs
@@ -1,10 +1,32 @@
+using ExitGames.Client.Photon;
 using Photon.Pun;
 using Photon.Realtime;
+using System;
 using UnityEngine;
+
+public enum EventCodes
+{
+    START_GAME,
+}
 
 public class NetworkHandler : MonoBehaviourPunCallbacks
 {
     private static byte playerCount;
+
+    #region Unity
+    public override void OnEnable()
+    {
+        base.OnEnable();
+        PhotonNetwork.NetworkingClient.EventReceived += OnEvent;
+    }
+
+    public override void OnDisable()
+    {
+        base.OnDisable();
+        PhotonNetwork.NetworkingClient.EventReceived -= OnEvent;
+    }
+    #endregion
+
     #region Public
 
     public static void StartConnectionAndJoinRoom()
@@ -15,20 +37,59 @@ public class NetworkHandler : MonoBehaviourPunCallbacks
 
     #endregion
 
+    #region Private
+    protected void RaisePhotonEvent(EventCodes eventCodes, object parameters, ReceiverGroup receiverGroup)
+    {
+        Debug.Log("RaisePhotonEvent" + eventCodes.ToString());
+        byte eventCode = (byte)eventCodes;
+        RaiseEventOptions raiseEventOptions = new RaiseEventOptions { Receivers = receiverGroup };
+        SendOptions sendOptions = new SendOptions { Reliability = true };
+        PhotonNetwork.RaiseEvent(eventCode, parameters, raiseEventOptions, sendOptions);
+    }
+    #endregion
+
     #region EventHandlers
     public override void OnConnectedToMaster()
     {
         base.OnConnectedToMaster();
         Debug.Log("Photon Connected");
-        PhotonNetwork.JoinRandomOrCreateRoom(null, LaddersConfig.MAX_PLAYERS);
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = LaddersConfig.MAX_PLAYERS;
+        roomOptions.PlayerTtl = 600;
+        roomOptions.EmptyRoomTtl = 600;
+        roomOptions.PublishUserId = true;
+        roomOptions.CleanupCacheOnLeave = true;
+        PhotonNetwork.JoinRandomOrCreateRoom(null, LaddersConfig.MAX_PLAYERS, MatchmakingMode.FillRoom, null, null, null, roomOptions);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        Debug.Log("Photon Joined Room");
     }
 
     public override void OnPlayerEnteredRoom(Player newPlayer)
     {
         base.OnPlayerEnteredRoom(newPlayer);
-        Debug.Log("Photon Joined Room");
-        playerCount++;
-        if(playerCount.Equals(LaddersConfig.MAX_PLAYERS))
+        Debug.Log("Photon Entered Room");
+        if (PhotonNetwork.CurrentRoom.PlayerCount == LaddersConfig.MAX_PLAYERS)
+        {
+            RaisePhotonEvent(EventCodes.START_GAME, "0", ReceiverGroup.All);
+        }
+    }
+
+    public override void OnCreatedRoom()
+    {
+        base.OnCreatedRoom();
+        Debug.Log("Photon Created Room");
+    }
+
+
+
+    private void OnEvent(EventData eventData)
+    {
+        byte eventCode = eventData.Code;
+        if (eventCode == (byte)EventCodes.START_GAME)
         {
             EventController.TriggerEvent(EventID.EVENT_PHOTON_ALL_PLAYERS_JOINED);
         }
